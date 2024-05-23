067759088be9bf934432aef85e6fffdae40e2629
Allex Veldman <allexveldman@gmail.com>
2024-05-23 16:34:48 +0200

    Add toml to treesitter

--- nvim/.config/nvim/lua/treesitter-setup.lua
+++ nvim/.config/nvim/lua/treesitter-setup.lua
@@ -4,7 +4,7 @@
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'json' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'json', 'toml', },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
@@ -68,7 +68,6 @@ vim.defer_fn(function()
 end, 0)
 
 -- Enable folding using treesitter
-vim.wo.foldmethod='expr'
-vim.wo.foldexpr='nvim_treesitter#foldexpr()'
-vim.wo.foldenable=false
-
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.wo.foldenable = false
